4a5235ba74aad2dfd0da7536bc16e53b88fd80bc
pacific <146333895+IT-pacific@users.noreply.github.com>
2025-11-28 13:44:19 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+// Test route
+app.get("/v1/server-status", (req, res) => {
+    console.log(req.url);
+    res.status(200).send("All services are active 👍");
+});
+
+app.listen(8000, () => "Server up and runnin!");

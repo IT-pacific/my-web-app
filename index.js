80778e0e489522bc8867b149b33432c3081767ab
pacific <146333895+IT-pacific@users.noreply.github.com>
2025-12-04 17:53:18 +0200

    Add user authentication feature

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 console.log("Hello, this is a basic web app!");
 
@@ -11,6 +12,28 @@ console.log("Hello, this is a basic web app!");
 app.get("/v1/server-status", (req, res) => {
     console.log(req.url);
     res.status(200).send("All services are active 👍");
+});
+
+const users = [
+    { id: 1, names: "Jane doe", email: "jane@xyz.io", password: "Pass45@#" },
+];
+
+app.post("/v1/login", (req, res) => {
+    const { email, password } = req.body;
+
+    const user = users.find(
+        (user) => user.email === email && user.password === password
+    );
+
+    if (!user) {
+        return res
+            .status(401)
+            .json({ Ok: false, message: "Invalid credentials." });
+    }
+    res.status(200).json({
+        Ok: true,
+        message: "Logged In your're very welcome!",
+    });
 });
 
 app.listen(3000, () => "Server up and runnin!");
